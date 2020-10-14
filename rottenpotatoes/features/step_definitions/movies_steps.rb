c2672a483e0b884cdf386081dfa12a5f49a0a0d3
EC2 Default User <ec2-user@ip-172-31-9-179.us-east-2.compute.internal>
2020-10-14 03:36:57 +0000

    Part 2 complete

--- rottenpotatoes/features/step_definitions/movies_steps.rb
+++ rottenpotatoes/features/step_definitions/movies_steps.rb
@@ -1,4 +1,3 @@
-
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     Movie.create movie
@@ -22,4 +21,9 @@ Then /I should see all the movies/ do
   Movie.all.each do |movie|
     step %{I should see "#{movie.title}"}
   end
+end
+
+Then /^the director of "(.*)" should be "(.*)"$/ do |title_value, director_value|
+  movie = Movie.find_by_title(title_value)
+  expect(movie.director).to eq(director_value)
 end
